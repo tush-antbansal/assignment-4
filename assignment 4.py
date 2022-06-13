53e999eca2504f8ea7dc0a99970ab634641e690e
tush-antbansal <101082652+tush-antbansal@users.noreply.github.com>
2022-06-13 10:18:25 +0530

    Update assignment 4.py

--- assignment 4.py
+++ assignment 4.py
@@ -22,7 +22,7 @@ elif mark >= 80:
 print('Q2')
 year = int(input('Enter year:'))
 if year % 4 == 0:
-    if year % 100 == 0:
+    if year % 100 == 0 and year%400 == 0:
         print("Leap year")
     else:
         print("Not a leap year")
